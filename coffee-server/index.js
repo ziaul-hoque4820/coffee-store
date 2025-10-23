d9263cf235d292c71d0e849b90ea6bccaf678029
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-10-23 21:29:01 +0600

    Add POST Method in server side

--- coffee-server/index.js
+++ coffee-server/index.js
@@ -29,6 +29,17 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const coffeeCollection = client.db("coffeeDB").collection('coffees')
+
+        app.post('/coffees', async (req, res) => {
+            const newCoffee = req.body;
+            console.log(newCoffee);
+            const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result);
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
