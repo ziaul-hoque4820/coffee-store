04939022a38eb42f7414b46bfb675a7da29a5bc6
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-10-27 07:37:03 +0600

    Firebase install and Add auto scroll top component

--- coffee-store-client/src/layout/MainLayout.jsx
+++ coffee-store-client/src/layout/MainLayout.jsx
@@ -3,10 +3,12 @@ import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 import FooterSection from '../components/FooterSection'
 import CopyRight from '../components/CopyRight'
+import ScrollToTop from '../components/ScrollToTop'
 
 function MainLayout() {
     return (
         <div>
+            <ScrollToTop />
             <Header></Header>
             <div className='max-w-7xl mx-auto'>
                 <Outlet></Outlet>
